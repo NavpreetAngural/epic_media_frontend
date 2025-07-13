dcef9e4778c68cf64f366ed9aca29c706072404c
Navpreet <navangural175@gmail.com>
2025-07-14 01:18:15 +0530

    portfolio layout manage

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-export const baseURL = 'http://localhost:3000/api';
+// export const baseURL = 'http://localhost:3000/api';
 
 
-// export const baseURL = 'https://epic-media-backend.onrender.com/api';
+export const baseURL = 'https://epic-media-backend.onrender.com/api';
 
 
