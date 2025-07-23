1401df74a208f27eb3e20bb16496549a3d4bb44f
Navpreet <navangural175@gmail.com>
2025-07-23 21:32:40 +0530

    about page fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
     }, [])
 
     const onFinish = async (values) => {
-        
+
         await axios.post(`${baseURL}/story/add`, values).then((res) => {
             toast.success(res.data.msg)
             form.resetFields();
@@ -40,25 +40,21 @@ const About = () => {
                 <div className='heading w-full md:w-full flex justify-center items-center h-[10em] lg:h-[10em] border-y lg:border-0 lg:m-0 mt-5' >
                     <h1 className='lg:text-5xl text-4xl'><i>About us</i></h1>
                 </div>
-                <div data-aos="fade-left" className='flex flex-col w-full lg:w-1/2 text-justify justify-start items-center lg:p-5' >
-                        <h1 className='text-2xl p-5 lg:p-0 italic'>Our Services</h1>
-                        <p className='lg:py-5 text-[12px] lg:text-[15px]'>Our work spans across various genres, yet the heart remains the same: visual storytelling with emotion and cinematic flair. Here’s what we offer:
-                            <ul className='p-5  text-center flex flex-col gap-3 text-[15px] font-semibold'>
-                                <li> 🎬 Cinematic Weddings & Pre-Weddings</li>
-                                <li> 🌍 Travel Films & Vlogs</li>
-                                <li> 🎶 Music & Song Videos</li>
-                                <li> 🎞️ Web Series & Short Films</li>
-                                <li> 🎥 BTS & Creative Shoots</li>
-                            </ul>
-                        </p>
-                    </div>
+                
                 <div className='flex flex-col lg:flex-row w-full lg:p-1 justify-center items-center'>
-                    <div data-aos="fade-right" className='flex flex-col w-full lg:w-1/2 text-justify justify-start items-center lg:p-5' >
-                        <h1 className='text-2xl p-5 lg:p-0 italic'>About Epic Media</h1>
-                        <p className='lg:py-5 text-[12px] lg:text-[15px]'>Welcome to Epic Media — where creativity meets storytelling, and every frame speaks louder than words.
-                            We are not just a photography and filmmaking brand — we are visual storytellers who believe in the power of moments. Founded out of sheer passion for art and cinematic expression, Epic Media was created to transform ordinary memories into extraordinary visual experiences. Through the lens of our cameras, we capture not just images, but the emotion, energy, and essence behind each story.
-                            Whether it's a couple’s first glance on their wedding day, the raw beauty of a remote Himalayan village, or the behind-the-scenes chaos of a music video shoot — we are there, preserving the moments that matter.</p>
-                    </div>
+                    <div data-aos="fade-left" className='flex flex-col w-full lg:w-1/2 text-justify justify-start items-center lg:p-5' >
+                    <h1 className='text-2xl p-5 lg:p-0 italic'>Our Services</h1>
+                    <p className='lg:py-5 text-[12px] lg:text-[15px]'>Our work spans across various genres, yet the heart remains the same: visual storytelling with emotion and cinematic flair. Here’s what we offer:
+                        <ul className='p-5  text-center flex flex-col gap-3 text-[15px] font-semibold'>
+                            <li> 🎬 Cinematic Weddings & Pre-Weddings</li>
+                            <li> 🌍 Travel Films & Vlogs</li>
+                            <li> 🎶 Music & Song Videos</li>
+                            <li> 🎞️ Web Series & Short Films</li>
+                            <li> 🎥 BTS & Creative Shoots</li>
+                        </ul>
+                    </p>
+                </div>
+                   
                     <div data-aos="fade-left" className='flex flex-col w-full lg:w-1/2 text-justify justify-center items-center lg:p-5' >
                         <h1 className='text-2xl p-5 lg:p-0 italic'>Our Story</h1>
                         <p className='lg:py-5 text-[12px] lg:text-[15px]'>The journey of Epic Media began with a simple idea: to share stories that are powerful, emotional, and visually stunning. What started as a small YouTube channel has evolved into a full-scale creative production house specializing in photography, cinematography, and digital storytelling.
@@ -68,6 +64,12 @@ const About = () => {
                     </div>
                 </div>
                 <div className='flex flex-col lg:flex-row w-full lg:p-1 justify-center items-center'>
+                     <div data-aos="fade-right" className='flex flex-col w-full lg:w-1/2 text-justify justify-start items-center lg:p-5' >
+                        <h1 className='text-2xl p-5 lg:p-0 italic'>About Epic Media</h1>
+                        <p className='lg:py-5 text-[12px] lg:text-[15px]'>Welcome to Epic Media — where creativity meets storytelling, and every frame speaks louder than words.
+                            We are not just a photography and filmmaking brand — we are visual storytellers who believe in the power of moments. Founded out of sheer passion for art and cinematic expression, Epic Media was created to transform ordinary memories into extraordinary visual experiences. Through the lens of our cameras, we capture not just images, but the emotion, energy, and essence behind each story.
+                            Whether it's a couple’s first glance on their wedding day, the raw beauty of a remote Himalayan village, or the behind-the-scenes chaos of a music video shoot — we are there, preserving the moments that matter.</p>
+                    </div>
                     <div data-aos="fade-right" className='flex flex-col w-full lg:w-1/2 text-justify justify-center items-center lg:p-5' >
                         <h1 className='text-2xl p-5 lg:p-0 italic'>What We Believe</h1>
                         <p className='lg:py-5 text-[12px] lg:text-[15px]'>At Epic Media, we believe photography is more than just clicking pictures. It’s about capturing a moment in time, preserving a feeling, and telling a story that speaks to the heart.
@@ -80,7 +82,7 @@ const About = () => {
                             Whether we are working with a bride on her big day, documenting the pulse of a music concert, or filming a travel vlog in the mountains — we give our 100% to ensure the end result is powerful, moving, and unforgettable.
                         </p>
                     </div>
-                    
+
                 </div>
                 <div className='flex flex-col lg:flex-row w-full lg:p-1 justify-center items-center '>
                     <div data-aos="fade-in" className='flex flex-col lg:w-[70%] text-justify justify-center items-center lg:p-5' >
